03eebd6c023df519d9d0968670dadbba015ecef4
limjiminn <jiaezzan@naver.com>
2021-02-15 13:56:28 +0900

    review

---
+++ Review_001/src/com/limjiminn/java/Review_07.java
@@ -0,0 +1,14 @@
+package com.limjiminn.java;
+
+/*
+ * for(int i = 0; i < 100; i++)반복문, if() 비교 연산 명령문
+ * Random 클래스를 사용하여 다음문제를 풀이하시오
+ * 
+ * 1 ~ 100 까지 임의의 숫자 100개를 만들고 100개 중에 짝수들 만의 합을 구하시오
+ */
+
+public class Review_07 {
+	public static void main(String args[]) {
+		
+	}
+}
